d0878ce6fc644acfa70b1549eb088cf3bf565869
O.Nsibu <oseedoublez@gmail.comgit config --global user.email oseedoublez@gmail.comgit config --global user.email oseedoublez@gmail.com>
2021-12-08 09:43:50 +0100

    1. Buchstaben prüfen.

--- DemoProject/src/WoerterRaten.java
+++ DemoProject/src/WoerterRaten.java
@@ -22,6 +22,12 @@ public class WoerterRaten {
 		Scanner eingabe = new Scanner(System.in); 
 		System.out.println("Dein Tipp");
 		String tipp = eingabe.next();
+		
+		if (tipp.charAt(0) == wort.charAt(0)) {
+			System.out.println("1.Buchstabe Richtig");
+		}else {
+			System.out.println("1. Buchstabe falsch");
+		}
 	}
 
 }
